8f10700b6e14847eec6c29419259ceb9f283080b
live-by-unix <262261208+live-by-unix@users.noreply.github.com>
2026-07-02 10:04:01 -0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+
+  server: {
+    host: true,
+  },
+
+  preview: {
+    host: true,
+  },
+
+  build: {
+    target: "esnext",
+  },
+});
