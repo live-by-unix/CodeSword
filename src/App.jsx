f3a75cc4cc5ba6e27eefc03edb663393efe1cbcc
live-by-unix <262261208+live-by-unix@users.noreply.github.com>
2026-07-02 10:07:00 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
